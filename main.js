638a0d6c3bd1f5fdb652057a53fe23ed23d50853
sanaycode <88113041+sanaycode@users.noreply.github.com>
2022-03-23 21:36:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+    //
+}
+function setup(){
+    var canvas = createCanvas(300, 300);
+    canvas.position(619, 250);
+}
+function draw(){
+    //
+}
+function takeSnapshot(){
+    save('mustachefilterimage.png');
+}
